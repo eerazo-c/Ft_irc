74f160d03e88bc76dd001da962af058450bc488b
Israel Torrico Castro <israetor@cbr5s1.42barcelona.com>
2026-04-09 19:33:27 +0200

    added gitignore , added test client

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int ar, char const *argv[])
     {
         std::cout << RED << "ERROR: Invalid Arguments" << RESET <<
         std::endl << YELLOW << "usage: /ircserv <port> <password>" << RESET<< std::endl;
-        return 0;
+        return 1;
     }
     std::cout << "Todo bien jeje" << std::endl;
     std::string in_port (argv[1]);
@@ -48,25 +48,38 @@ int main(int ar, char const *argv[])
     // sockaddr_in  struct para ipv4 donde podemos añadir ip y puerto
     //  hacemos cast de sockaddr_in a sockaddr porque lo pide la funcion concreta
 
-    // Atar el socket al puerto pasado por parametro   ej 8080
+    // Ata el socket al puerto pasado por parametro   ej 8080
     if (bind(socket_t, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
         std::cerr << "Error in bind;" << std::endl;
+        return 1;
     }
     
     // Ponerse a escuchar (máximo 3 personas en fila)
-    listen(socket_t, 3);
+    if (listen(socket_t, 3) < 0)
+    {
+        std::cerr << "Error in listen;" << std::endl;
+        return 1;
+    }
     std::cout << "Esperando cliente..." << std::endl;
 
      // Aceptar la llamada (esto bloquea el programa hasta que alguien se conecta)
     int addrlen = sizeof(server_address);
     int new_socket = accept(socket_t, (struct sockaddr *)&server_address, (socklen_t*)&addrlen);
-
+    if (new_socket  < 0)
+    {
+        std::cerr << "Error in accept" << std::endl;
+        return 1;
+    }
     std::cout << "¡Cliente conectado!" << std::endl;
 
     // Leer lo que mandó el cliente
     char buffer[1024] = {0};
-    recv(new_socket, buffer, 1024, 0);
+    if (recv(new_socket, buffer, 1024, 0))
+    {
+        std::cerr << "Error in recv" << std::endl;
+        return 1;
+    }
     std::cout << "Mensaje recibido: " << buffer << std::endl;
 
     close(new_socket);
