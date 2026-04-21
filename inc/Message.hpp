f86c3c859070eef9fa09bbb2f670c2716460d544
Elizabeth Nayeli Erazo Chacon <elerazo-@car8s2.42barcelona.com>
2026-04-21 20:22:41 +0200

    Message.hpp add los define

--- inc/Message.hpp
+++ inc/Message.hpp
@@ -14,13 +14,30 @@
 //networkname = servidor en 001
 
 //Bienvenida + info + se crean cosas
-#define RPL_WELCOME(nick, server) "001" + (nick) + ":Welcome to the INEA Network," + (server)
-#define RPL_YOURHOST(nick, server, version) "002" + (nick) + ":Your host is " + (server) + "running version" + (version)
-#define RPL_CREATED(server) "003" + (client) + ":This server was created" + (datetime)
-#define RPL_MYINFO(server) "004" + (client) + (servername) + (version) + (available user modes) + (available channel modes)"
-#define RPL_ISUPPORT(server) "005" + (client) + (1-13 tokens) + ":are supported by this server"
+#define RPL_WELCOME(nick, server) "001 " + (nick) + ":Welcome to the INEA Network," + (server)
+#define RPL_YOURHOST(nick, server) "002 " + (nick) + ":Your host is " + (server) + "running version IRC42"
+#define RPL_CREATED(nick, datetime) "003 " + (nick) + ":This server was created" + (datetime)
+#define RPL_MYINFO(nick, server) "004 " + (nick) + (server)
+#define RPL_UMODEIS(nick, modes) "221 " +  (nick) + (modes)
+#define RPL_CHANNELMODEIS(client, channel, modes) "324 " +  (client) + (channel) + (modes)
+#define RPL_NOTOPIC(nick, channel) "331 " + (nick) + (channel) + ":No topic is set"
+#define RPL_TOPIC(nick, channel) "332 " + (nick) + (channel) + ":<topic>"
+#define RPL_INVITING(nick, channel) "341 " +  (nick) + (channel)
 
 // Errores
-#define ERR_NOSUCHNICK(server) "401" + (client) + (nickname) + ":No such nick/channel" 
-#define ERR_NOSUCHCHANNEL(server) "403" + (client) + (channel) + ":No such channel"
-#define ERR_CANNOTSENDTOCHAN(server) "404" + (client) + (channel) + ":Cannot send to channel"
+#define ERR_NOSUCHNICK(nick) "401 " + (nick) + ":No such nick/channel" 
+#define ERR_NOSUCHCHANNEL(nick, channel) "403 " + (nick) + (channel) + ":No such channel"
+#define ERR_CANNOTSENDTOCHAN(nick, channel) "404 " + (nick) + (channel) + ":Cannot send to channel"
+#define ERR_NORECIPIENT(nick, command) "411 " + (nick) + ":No recipient given" (command)"
+#define ERR_NOTEXTTOSEND(nick) "412 " + (nick) + ":No text to send"
+#define ERR_NOTREGISTERED(nick) "451 " +  (nick) + ":You have not registered"
+#define ERR_NEEDMOREPARAMS(nick, command) "461 " + (nick) + (command) + ":Not enough parameters"
+#define ERR_ALREADYREGISTERED(nick) "462 " + (nick) + ":You may not reregister"
+#define ERR_CHANNELISFULL(nick, channel) "471 " + (nick) + (channel) + ":Cannot join channel (+l)" 
+#define ERR_UNKNOWNMODE(nick, modechar) "472 " + (nick) + (modechar) + ":is unknown mode char to me"
+#define ERR_INVITEONLYCHAN(nick, channel) "473 " + (nick) + (channel) + ":Cannot join channel (+i)"
+#define ERR_BADCHANNELKEY(nick, channel) "475 " + (nick) + (channel) + ":Cannot join channel (+k)"
+#define ERR_NOCHANMODES(nick, channel) "477 " + (nick) + " " + (channel) + " :Channel doesn't support modes"
+#define ERR_CHANOPRIVSNEEDED(nick) "482 " +  (nick) +  (channel) + ":You're not channel operator"
+#define ERR_USERSDONTMATCH(nick) "502 " + (nick) + ":Cant change mode for other users" 
+#define ERR_INVALIDMODEPARAM() "696 " + (client) + (target) + (modechar) + (parameter) + ":<description>"
