7f7aa4f24f8221f9284aea1daba3fd406830175d
Elizabeth Nayeli Erazo Chacon <elerazo-@car11s3.42barcelona.com>
2026-05-04 20:17:56 +0200

    deje la funcion con container

--- src/ChannelManager.cpp
+++ src/ChannelManager.cpp
@@ -37,3 +37,11 @@ void ChannelManager::partAll(Client &client)
 	for (it = _channel.begin(); it != _channel.end(); ++it)
 		Part(client, it->first, "");
 }
+/*
+Channel *ChannelManager::Search(const std::string &name)
+{
+	std::map<std::string, Channel *>::iterator it = _channels.find(name);
+	if (it != _channels.end())
+		return (it->second);
+	return (0);
+}*/
