792cf9fffbe9607982ed1a81b6edd60399cbbea8
Ariel Rhea <arhea@car10s4.42barcelona.com>
2026-05-15 18:14:03 +0200

    INVITE correction

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: arhea <arhea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 17:48:43 by elerazo-          #+#    #+#             */
-/*   Updated: 2026/05/14 17:52:55 by arhea            ###   ########.fr       */
+/*   Updated: 2026/05/15 18:03:48 by arhea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -508,6 +508,7 @@ void Invite::execute(Client& client, std::vector<std::string> args, Server &serv
 	client.WritePrefix(RPL_INVITING(client.getNick(), channelName, targetNick));
 	target->WritePrefix(":" + client.getNick() + "!" + client.getUser()
 		+ "@localhost INVITE " + targetNick + " :" + channelName);
+    server.enableSendEvent(server.getEpoll_fd(), target->getFd());
 }
 
 Topic::~Topic()
